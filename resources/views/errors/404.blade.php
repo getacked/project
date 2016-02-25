1d8ff5eb5a250b2d88d94ab0d2868eadb4f29ee8
Jake Biatch <jakebwalsh@gmail.com>
2016-02-25 17:57:25 +0000

    404 Page a bit sexy

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,9 +8,31 @@
 @section('content')
 
   <div class="container flow-text">
-    <h5>Sorry, the page you're trying to access cannot be reached right now.</h5>
-
-    <h5> Please come back soon and try again!</h5>
+    <br>
+    <h5 class="center-align">Sorry, the page you're trying to access cannot be reached right now.</h5>
+    <div class="row valign-wrapper">
+      <div class="divider col s4 valign"></div>
+      <div class="col s4 center-align">
+        <i class="material-icons large">error</i>
+      </div>
+      <div class="divider col s4 valign"></div>
+    </div>
+    <h5 class="center-align">It is possible this page isn't here.</h5>
+    <p>
+      If you feel the page you're looking for should be here feel free to contact us
+      from the link below...
+    </p>
+    <div class="row valign-wrapper">
+      <div class="divider col s4 valign"></div>
+      <div class="col s4 center-align">
+       <a href="{{ route('contact') }}" class="waves-effect waves-light btn">Contact Us</a>
+      </div>
+      <div class="divider col s4 valign"></div>
+    </div>
+    <p>
+      In the mean time, why not try going <a href="{{ route('landing') }}">home</a> and 
+      seeing what other cool pages we have to show you!
+    </p>
   </div>
 
 @stop
